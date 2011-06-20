262114fb94c7cacfee03ba616f86db136190a7be
marcovena <marcovena@libero.it>
2011-06-20 23:27:49 +0200

    Fix dei margini in photos.html pulizia css
    eliminazione riferimenti js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 	//SLIDER GALLERY	
-	$('.slideshow').cycle({
+/*	$('.slideshow').cycle({
 		fx: 'fade',
 		next: '#gallery-holder .navigation a.next',
 		prev: '#gallery-holder .navigation a.prev'
 	});
-	
+*/	
 	//fade in of the main menu
 	
 	$('.sub-menu').hide().delay(1000).slideDown('slow');
