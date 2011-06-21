35c70d8fb7a84546085c01e851f49c46f726639f
marcovena <marcovena@libero.it>
2011-06-21 18:10:06 +0200

    Contact stuff()

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
-$(document).ready(function() {
-	//SLIDER GALLERY	
-/*	$('.slideshow').cycle({
-		fx: 'fade',
-		next: '#gallery-holder .navigation a.next',
-		prev: '#gallery-holder .navigation a.prev'
+$(document).ready( function() {
+	//SLIDER GALLERY
+	/*	$('.slideshow').cycle({
+	fx: 'fade',
+	next: '#gallery-holder .navigation a.next',
+	prev: '#gallery-holder .navigation a.prev'
 	});
-*/	
+	*/
 	//fade in of the main menu
-	
+
 	$('.sub-menu').hide().delay(1000).slideDown('slow');
-	
+
+	//$("#enquiry-form").validate();
+
+	$('#enquiry-form').validate({
+		submitHandler: function(form) {
+			$(form).ajaxSubmit({
+				url: 'process.php',
+				success: function() {
+					//$('#contact').hide();
+					$('enquiry-form').append("<p class='thanks'>Grazie! Il tuo messaggio e' stato inviato.</p>")
+				}
+			});
+		}
+	});
 });
-
-
