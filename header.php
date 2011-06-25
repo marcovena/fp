e0079d40fb52843892c4322e41299ee4b592f579
marcovena <marcovena@libero.it>
2011-06-25 23:14:30 +0200

    first changes file ranamed exported header

---
+++ header.php
@@ -0,0 +1,14 @@
+<?php
+
+echo '<head>
+		<title>Francesco Paoli Fine Art Photography - Home</title>
+		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+
+		<link rel="shortcut icon" href="images/fp-fav.ico">
+
+		<link rel="stylesheet" type="text/css" href="css/all.css" media="screen" />
+	</head>
+';
+
+
+?>
