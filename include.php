b23fbcb2ddef9566c13fbc4fec30eb4b4fe836bd
marcovena <marcovena@libero.it>
2011-06-28 18:27:51 +0200

    restore header

--- include.php
+++ include.php
@@ -1,49 +1,68 @@
 <?php
 
-
 /* get the head for the html */
 
-function get_head(){
-echo '<head>
-		<title>Francesco Paoli Fine Art Photography - Home</title>
-		<meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
+function get_head() {
+    echo '<head>
+        <title>Francesco Paoli Fine Art Photography - Home</title>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" >
        <meta content="web photography portfolio francesco paoli" />
-		<link rel="shortcut icon" href="images/fp-fav.ico">
+        <link rel="shortcut icon" href="images/fp-fav.ico">
 
-		<link rel="stylesheet" type="text/css" href="css/all.css" media="screen" />
-	</head>';
+        <link rel="stylesheet" type="text/css" href="css/all.css" media="screen" />
+    </head>';
 }
 
 /* get the header for all the pages */
-function get_header_bar(){
-    
+function get_header_bar($forPage) {
+
     echo '<div id="header">
-                <div id="header-brand">
+                <a class="home-logo" href="index2.php" title="index2"> <div id="header-brand">
                     <h2>FRANCESCO PAOLI</h2>
                     <div id="header-divider">
                     </div>
                     <h4>fine art photography</h4>
-                </div>
+                </div></a>               
+    
                 <ul class="nav-menu">
                     <li>
-                        <a href="photos.php">Foto</a>
+                        <a href="photos.php" ',  addActive($forPage, 'photos'), ' >Foto</a>
+                        <ul class="sub-menu">
+
+                            <li>
+                                <a class="active" href="#">Limited Edition Pano</a>
+                            </li>
+                            <li>
+                                <a href="#">Landscapes</a>
+                            </li>
+                            <li>
+                                <a href="#">Various</a>
+                            </li>
+                        </ul>
                     </li>
                     <li>
-                        <a href="info.php">Info</a>
+                        <a href="info.php" ',  addActive($forPage, 'info'), ' >Info</a>
                     </li>
                     <li>
-                        <a href="contact.php">Contatti</a>
+                        <a href="contact.php" ',  addActive($forPage, 'contact'), ' >Contatti</a>
                     </li>
                 </ul>
             </div> ';
-    
+
 }
 
 /* get the footer for the pages */
-function get_footer(){
+
+function get_footer() {
     echo '<div id="footer" >&copy; All rights reserved - Francesco Paoli 2011</div>';
-    
+
 }
 
+function addActive($forPage, $placeholder) {
 
+    if($forPage == $placeholder) {
+        echo 'class="active"';
+
+    }
+}
 ?>
