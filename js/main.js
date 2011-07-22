12a87409f21fd8cd0b0aaeefe39c367d1db0785f
marcovena <marcovena@libero.it>
2011-07-22 19:13:48 +0200

    style alternativo + primi cambi che funzionano

--- js/main.js
+++ js/main.js
@@ -1,18 +1,25 @@
 $(document).ready( function() {
 	$('.sub-menu').hide().delay(300).fadeIn(400);
 	
-	initStyleswitcher();
+	initStyleSwitcher();
 	
 });
 
 //create a toggle switcher that will toggle the site theme from dark to white
 function initStyleSwitcher(){
-  var styleSwitcher = '<div id="style-switcher" <ul><li><a id="css-red" href="#red">Red</a></li><li><a id="css-blue" href="#blue">Blue</a></li>' + 
-                      ' <li><a id="css-green" href="#green">Green</a></li></ul></div>';
+  var styleSwitcher = '<div id="style-switcher" class="not-selectable"><ul><li id="light-style-switch" class="shadow">Light</li><li id="dark-style-switch" class="shadow">Dark</li></ul></div>';
                       
        $('body').append(styleSwitcher);
-       
-
-
-
+ 
+        $('#dark-style-switch').click(function(){
+            
+            $("link[rel=stylesheet]").attr({href : "css/all-black.css"});
+            
+        });
+        
+        $('#light-style-switch').click(function(){
+            
+            $("link[rel=stylesheet]").attr({href : "css/all.css"});
+            
+        });
 }
