1475de7fb19b58cdfd9cf0b1126b38db05a6ffa1
marcovena <marcovena@libero.it>
2011-07-22 17:54:23 +0200

    init toggle switch

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(document).ready( function() {
 	$('.sub-menu').hide().delay(300).fadeIn(400);
+	
+	initStyleswitcher();
+	
 });
+
+//create a toggle switcher that will toggle the site theme from dark to white
+function initStyleSwitcher(){
+  var styleSwitcher = '<div id="style-switcher" <ul><li><a id="css-red" href="#red">Red</a></li><li><a id="css-blue" href="#blue">Blue</a></li>' + 
+                      ' <li><a id="css-green" href="#green">Green</a></li></ul></div>';
+                      
+       $('body').append(styleSwitcher);
+       
+
+
+
+}
