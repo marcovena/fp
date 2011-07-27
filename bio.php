dadb294f049ce9fdcdac6f6b4437db10a502b7a2
marcovena <marcovena@libero.it>
2011-07-27 19:13:02 +0200

    realizzata pagina bio.php

---
+++ bio.php
@@ -0,0 +1,41 @@
+<?php
+require_once ('include.php');
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+    <head>
+        <?php get_head('bio');?>
+    </head>
+    <body id="bio">
+        <div id="wrapper">
+            <?php get_header_bar('bio');?>
+            <div id="bio-wrapper">
+                <div class="bio-text-content">
+                    <p>
+                        Nato nel 1980 a Roma, inizia gli studi presso la Scuola Romana di Fotografia dove approfondisce le propria tecnica attraverso un intenso utilizzo di attrezzatura a pellicola, sviluppando e stampando in autonomia le proprie foto.
+                    </p>
+                    <p>
+                        Pur realizzando tra gli altri lavori anche reportage geografici e di viaggio, si specializza ben presto nella fotografia paesaggistica, dove concentra la maggiorparte del proprio impegno.
+                    </p>
+                    <p>
+                        Utilizza attrezzatura digitale, ma predilige per gli scatti di paesaggio l’uso di una macchina panoramica grande formato, scansionando le immagini con scanner a tamburo ad alta risoluzione e curando personalmente l’intero processo che va dalla postproduzione alla stampa delle sue intera collezione fotografica,
+                    </p>
+                    <p>
+                        Nel 2011 ottiene il riconoscimento Bronze Award al concorso Epson international pano award per tre delle fotografie presentate.
+                    </p>
+                </div>
+                <div class="bio-divider"></div>
+                
+                    <img class="bio-image-content" src="images/bio-image-content.jpg" alt="bio image content" title="Epson Awarded Photo"/>
+              
+                
+                <div class="clearfix"></div>
+            </div><!-- END BIO-WRAPPER -->
+            <div id="footer">
+                &copy; All rights reserved - Francesco Paoli 2011 
+            </div>
+        </div><!-- END BIO-WRAPPER -->
+    </body>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+</html>
