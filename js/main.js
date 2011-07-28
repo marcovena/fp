ce3ceecaaa20a7cbbd9f165188534224c103cecd
marcovena <marcovena@libero.it>
2011-07-28 19:15:30 +0200

    Animazione su immagine bio

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
-
+    
+    //enable header animation
+    if($('#header').length){
     $('.sub-menu').hide().delay(300).fadeIn(400);
-
+    }
+    
+    
     //enable theme switcher
     var loc = window.location;
     var enableString = "toggle=1";
@@ -9,12 +13,22 @@ $(document).ready(function() {
         initStyleSwitcher();
     }
 
-  /*  $('ul.sub-menu li a').each(function() {
-        if($(this).hasClass('active') == false) {
-            this.style.color = 'red';
-        }
-    });*/   
+    //fade bio image in
+     if($('body#bio').length){
+    $('img.bio-image-content').animate({
+    opacity: 1,
+    
+    height: 'toggle'
+  }, 1500, function() {
+    // Animation complete.
+  });
+       
+    }
+     
 });
+
+
+
 //create a toggle switcher that will toggle the site theme from dark to white
 function initStyleSwitcher() {
     var styleSwitcher = '<div id="style-switcher" class="not-selectable"><ul><li id="light-style-switch" class="shadow">Light</li><li id="dark-style-switch" class="shadow">Dark</li></ul></div>';
