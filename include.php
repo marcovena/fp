fb43eb8ad2b5d7eaafe92bb161bdec841fe88eb0
marcovena <marcovena@libero.it>
2011-08-01 18:00:04 +0200

    fix per favicon

--- include.php
+++ include.php
@@ -29,9 +29,9 @@ function get_head($suff) {
           <!-- Mobile viewport optimized: j.mp/bplateviewport -->
           <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-          <link rel="shortcut icon" href="/favicon.ico"> 
-          <link rel="apple-touch-icon" href="/apple-touch-icon.png">
-          <link rel="stylesheet" href="css/style.css?v=2">
+          <link rel="shortcut icon" href="favicon.ico"> 
+          <link rel="apple-touch-icon" href="apple-touch-icon.png">
+          <link rel="stylesheet" href="css/style.css?v=3">
           
       <!--   <link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />  -->
       <!--    <link rel="stylesheet" type="text/css" href="css/mobile.css" media="only screen and (min-width:200px) and (max-width:480px)"/> -->
