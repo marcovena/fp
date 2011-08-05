5abb4e3d1b17050324aa0bfb73104bdea33f8740
marcovena <marcovena@libero.it>
2011-08-05 15:38:52 +0200

    Revisione contact.php

--- process.php
+++ process.php
@@ -1,94 +1,103 @@
 <?php
 
+//configurazione prod
+$smtp_server = 'smtp.francescopaoli.com';
+$username = '2933995@aruba.it';
+$password = 'cun2ax4j';
+$target_email = "info@francescopaoli.com";
+
+//configurazione dev
+//$smtp_server = 'mail.marcovenanzi.com';
+//$username = 'hi@marcovenanzi.com';
+//$password = 'benebene';
+//$target_email = "marcovena@libero.it";
+
+
+//testo e campi statici mail
+$subject = "Richiesta informazioni - www.francescopaoli.com";
+
+ 
+
+//enable debug
 $DEBUG = 0;
 
 if($DEBUG) {
-    echo "email: $email \n $message \n $target_email";
+    error_reporting(E_ALL);
 }
 
 if(isset($_POST['submit'])) {
 
-//need to validate 
-
-    $smtp_server = 'smtp.francescopaoli.com';
-    $username='2933995@aruba.it';
-    
-    $password = 'cun2ax4j';
-
     $name_field = trim($_POST['name']);
     $email_field = trim($_POST['email']);
     $website = trim($_POST['web']);
-    
-    $subject = "Richiesta informazioni - www.francescopaoli.com";
-    
-//owner email
-   // $target_email = "marcovena@libero.it";
-    $target_email = "info@francescopaoli.com";
 
     $msg = trim($_POST['text']);
 
+    
+    echo 'Validation start';
     //validate name
-    if($name_field == ''){
+    if(empty($name_field)) {
         $hasError = true;
-    }
-     else {
+        echo 'Name field is empty';
+    } else {
         $name_field_san = $name_field;
     }
-     
-     //validate email
-    if($email_field == '')  {
-        $hasError = true;
-    } else if (!eregi("^[A-Z0-9._%-]+@[A-Z0-9._%-]+\.[A-Z]{2,4}$", $email_field)) {
-        $hasError = true;
-    } else
-        {
-            $email_san = $email_field;
-        }
 
- //validate msg
-    if($msg == '') {
+    //validate email
+    if(empty($email_field)) {
         $hasError = true;
+        echo 'Mail field is empty';
+    } elseif(0 == preg_match("/^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/", $email_field)) {
+        $hasError = true;
+        echo 'There is an error in the mail formatting: , $email_field';
+    } else {
+        $email_san = $email_field;
     }
-    
-    if(!isset($hasError)){
 
-    $body = "<strong>From: </strong> $name_field_san </br> <strong>E-Mail:</strong> $email_san </br> 
+    //validate msg
+    if(empty($msg)) {
+        $hasError = true;
+        echo 'Mail Body is empty';
+    }
+
+    if(!isset($hasError) || $hasError == false) {
+        
+        $body = "<strong>From: </strong> $name_field_san </br> <strong>E-Mail:</strong> $email_san </br> 
             <strong>Sito Web: </strong> $website </br> <strong>Messaggio: </strong>$msg </br>";
 
+        if($DEBUG) {
+                var_dump($body);
+            echo "\n ending....";
+        }
+        //send email
 
+        require_once 'lib/swiftmailer/lib/swift_required.php';
 
+        if($DEBUG) {
+            echo "Loaded swiftmailer library";
+        }
 
-    if($DEBUG) {
-        echo "\n ending....";
+        //Create the Transport
+
+        $transport = Swift_SmtpTransport::newInstance($smtp_server, 25) -> setUsername($username) -> setPassword($password);
+
+        //Create the Mailer using your created Transport
+        $mailer = Swift_Mailer::newInstance($transport);
+
+        //Create a message
+        $message = Swift_Message::newInstance($subject) -> setFrom(array($email_san => $name_field_san)) -> setTo(array($target_email => 'Francesco Paoli Photography')) -> setBody($body) -> setContentType("text/html");
+
+        //Send the message
+        $result = $mailer -> send($message);
+        echo 'Sent mail';
+        if($DEBUG) {
+            echo "Email Result: $result";
+        }
+        return $result;
+    }else {
+        echo "<h6> Attenzione! Campi non validi. Ricontrollare il form.</h6>";
     }
-    //send email
-
-    require_once 'lib/swiftmailer/lib/swift_required.php';
-    
-    if($DEBUG) {
-        echo "Loaded swiftmailer library";
-    }
-
-    //Create the Transport
-    
-    $transport = Swift_SmtpTransport::newInstance($smtp_server, 25) -> setUsername($username) -> setPassword($password);
-
-    //Create the Mailer using your created Transport
-    $mailer = Swift_Mailer::newInstance($transport);
-
-    //Create a message
-    $message = Swift_Message::newInstance($subject) 
-    -> setFrom( array($email_san => $name_field_san)) -> setTo( array($target_email => 'Francesco Paoli Photography')) -> setBody($body)
-    -> setContentType("text/html");
-
-    //Send the message
-    $result = $mailer -> send($message);
-    if($DEBUG) {
-        echo "Email Result: $result";
-    }
-    return 1;
-}
 } else {
-    echo "Error while sending email";
+    echo "Errore. Riprovare.";
 }
 ?>
