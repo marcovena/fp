c55d9d3da7434e2c6e5ea8e33a65597cdc77c227
marcovena <marcovena@libero.it>
2011-08-09 17:31:28 +0200

    Rimuove index.html cambia il colore dei link nella header

--- include.php
+++ include.php
@@ -19,9 +19,7 @@ function get_head($suff) {
 
           <!-- Always force latest IE rendering engine (even in intranet) & Chrome Frame
           Remove this if you use the .htaccess -->
-          <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">' .
-          
-          '<title>Francesco Paoli Fine Art Photography', $toAdd, ' </title>
+          <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">' . '<title>Francesco Paoli Fine Art Photography', $toAdd, ' </title>
           
           <meta name="description" content="">
           <meta name="author" content="Francesco Paoli | Fine Art Photography">
@@ -42,6 +40,8 @@ function get_head($suff) {
 /* get the header for all the pages */
 function get_header_bar($forPage) {
 
+    $gallery_id = -1;
+
     /*echo '<div id="header" class="not-selectable">
      <a class="home-logo " href="index2.php" title="index2">
      <div id="header-brand">
@@ -51,25 +51,24 @@ function get_header_bar($forPage) {
      <h4>fine art photography</h4>
      </div></a> */
 
-    $gallery_id = 0;
-    if(isset($_GET) && (!empty($_GET))) {
-        $gallery_id = $_GET['id'];
-
-    }
-
     echo '<div id="header" class="not-selectable">
                 <a class="home-logo" href="index2.php" title="index2"> 
                 
                 </a><div id="header-divider"></div>
                 <ul class="nav-menu">
                     <li>
-                        <a href="photos.php" ',      addActive($forPage, 'photos'), ' >Foto</a>';
+                        <a href="photos.php" ',       addActive($forPage, 'photos'), ' >Foto</a>';
 
     if($forPage == 'photos' || $forPage == 'gallery') {
+        if($forPage == 'gallery') { 
+            if(isset($_GET) && (!empty($_GET))) {
+                $gallery_id = $_GET['id'];
+            }
+        }
         echo '<ul class="sub-menu">
 
-                            <li>
-                                <a ' . ($gallery_id == 0 ? 'class="active"' : null) . ' href="gallery.php?id=0">Pano Orizzontali</a>
+        <li>
+        <a ' . ($gallery_id == 0 ? 'class="active"' : null) . ' href="gallery.php?id=0">Pano Orizzontali</a>
                             </li>
                             <li>
                                 <a ' . ($gallery_id == 1 ? 'class="active"' : null) . ' href="gallery.php?id=1">Pano Verticali</a>
@@ -82,13 +81,13 @@ function get_header_bar($forPage) {
 
     echo '</li>
                     <li>
-                        <a href="info.php" ',  addActive($forPage, 'info'), ' >Info</a>
+                        <a href="info.php" ',   addActive($forPage, 'info'), ' >Info</a>
                     </li>
                     <li>
-                        <a href="bio.php" ', addActive($forPage, 'bio'), ' >Bio</a>
+                        <a href="bio.php" ',  addActive($forPage, 'bio'), ' >Bio</a>
                     </li>
                     <li>
-                        <a href="contact.php" ', addActive($forPage, 'contact'), ' >Contatti</a>
+                        <a href="contact.php" ',  addActive($forPage, 'contact'), ' >Contatti</a>
                     </li>
                 </ul>
             </div> ';
@@ -102,9 +101,9 @@ function get_footer() {
 
 }
 
-function get_scripts_tag(){
- 
-   echo '<script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.js"></script>
+function get_scripts_tag() {
+
+    echo '<script src="//ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.js"></script>
   <script>window.jQuery || document.write("<script src=\'js/libs/jquery-1.5.1.min.js\'>\x3C/script>");</script>
   <!-- scripts concatenated and minified via ant build script-->
   <script src="js/plugins.js"></script>
@@ -123,15 +122,15 @@ function get_scripts_tag(){
     g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
     s.parentNode.insertBefore(g,s)}(document,"script"));
   </script>';
-    
+
 }
 
 function addActive($forPage, $placeholder) {
 
     if($forPage == $placeholder || ($forPage == "gallery" && $placeholder == 'photos')) {
-        
+
         echo 'class="active"';
     }
-        
+
 }
 ?>
